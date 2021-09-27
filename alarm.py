15e057bc067ba9e0b6360ab3be41ceedd7343406
Emil Djupvik <emil14833@gmail.com>
2021-09-28 00:19:26 +0200

    add error handling

--- alarm.py
+++ alarm.py
@@ -1,26 +1,33 @@
-import RPi.GPIO as GPIO
-from time import sleep
-from datetime import datetime, timedelta
+try:
 
-GPIO.setmode(GPIO.BOARD)
+    import RPi.GPIO as GPIO
+    from time import sleep
+    from datetime import datetime, timedelta
 
-from speaker import *
-from sensor import *
+    GPIO.setmode(GPIO.BOARD)
 
-with open('alarm_start.txt', 'r') as f:
-    time_string = f.read().strip()
-    time = datetime.strptime(time_string, '%d/%m/%Y %H:%M')
-    duration = time - datetime.now() - timedelta(minutes=40)
+    from speaker import *
+    from sensor import *
 
-    if duration > timedelta(hours=12):
-        print(f'next alarm at {time} is too long to wait. Aborting')
-        quit()
+    with open('alarm_start.txt', 'r') as f:
+        time_string = f.read().strip()
+        time = datetime.strptime(time_string, '%d/%m/%Y %H:%M')
+        duration = time - datetime.now() - timedelta(minutes=30)
 
-    print(f'Alarm will fire in {duration}')
+        if duration > timedelta(hours=12):
+            print(f'next alarm at {time} is too long to wait. Aborting')
+            GPIO.cleanup()
+            quit()
 
-    sleep(duration.total_seconds())
+        print(f'Alarm will fire in {duration}')
 
-curr = sensor()
-alarm_until(lambda : sensor() != curr)
+        sleep(duration.total_seconds())
 
-GPIO.cleanup()
+    curr = sensor()
+    alarm_until(lambda : sensor() != curr)
+
+    GPIO.cleanup()
+
+except Exception as e:
+    GPIO.cleanup()
+    print(e)
